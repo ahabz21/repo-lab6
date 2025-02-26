95addf46466e9869047aa15628fdda4b7e029805
Ali Habib <144078908+ahabz21@users.noreply.github.com>
2025-02-26 23:11:58 +0000

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,8 @@
 
 class Main {
 	public static void main(String [] m) {
-	System.out.println(“hello world”);
 	System.out.println(“conflict!”);
 	System.out.println(“bye world”);
-
 	return;
 	}
 }
